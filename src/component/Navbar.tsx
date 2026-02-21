fd548dcd14c005325f707976200432ec60ea4706
EPAV26-8D <leo.yukio.tanaka@gmail.com>
2026-02-21 12:46:34 -0300

    .

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -6,7 +6,7 @@ export function Navbar() {
       <nav className={styles.navbar} aria-label="Navegação principal">
         <ul className={styles.navLinks}>
           <li>
-            <button className="openAside">
+            <button className="openSidebar">
               <i className="bi bi-list"></i>
             </button>
           </li>

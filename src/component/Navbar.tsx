a4a8df1bb15e03d7fb21bf0b0e5f3f8665514dc0
EPAV26-8D <leo.yukio.tanaka@gmail.com>
2026-02-20 22:16:44 -0300

    .

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -4,10 +4,6 @@ export function Navbar() {
   return (
     <header className={styles.header}>
       <nav className={styles.navbar} aria-label="Navegação principal">
-        <a href="/" className={styles.logo}>
-          Insighter
-        </a>
-
         <ul className={styles.navLinks}>
           <li>
             <a href="/">Home</a>

1b2c8b943933e37385cbd5799661d0af6044a185
EPAV26-8D <leo.yukio.tanaka@gmail.com>
2026-03-02 20:14:36 -0300

    .

--- src/component/Sidebar.tsx
+++ src/component/Sidebar.tsx
@@ -10,12 +10,50 @@ export function Sidebar({ isOpen }: SidebarProps) {
       className={`${styles.sidebar} ${isOpen ? styles.open : ''}`.trim()}
       aria-hidden={!isOpen}
     >
+      <h5>Categorias</h5>
       <ul>
         <li>
-          <a href="/">Início</a>
+          <a href="">Bovinos</a>
         </li>
         <li>
-          <a href="/settings">Configurações</a>
+          <a href="">Suínos</a>
+        </li>
+        <li>
+          <a href="">Aves</a>
+        </li>
+        <li>
+          <a href="">Pescados</a>
+        </li>
+        <li>
+          <a href="">Entradas</a>
+        </li>
+        <li>
+          <a href="">Acompanhamentos</a>
+        </li>
+        <li>
+          <a href="">Sobremesas</a>
+        </li>
+        <li>
+          <a href="">Bebidas</a>
+        </li>
+      </ul>
+
+      <h5>Ocasiões</h5>
+      <ul>
+        <li>
+          <a href="">Churrasco</a>
+        </li>
+        <li>
+          <a href="">Almoço do Dia a Dia</a>
+        </li>
+        <li>
+          <a href="">Almoço Especial</a>
+        </li>
+        <li>
+          <a href="">Jantar do Dia a Dia</a>
+        </li>
+        <li>
+          <a href="">Jantar Especial</a>
         </li>
       </ul>
     </aside>
